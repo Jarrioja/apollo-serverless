379244ff2991c0014b91288570f6f15101e005d8
Jesus Arrioja <Jarrioja2210@gmail.com>
2022-10-06 18:42:41 -0300

    fix gql deploy

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,20 +1,15 @@
+require("dotenv").config();
 const { ApolloServer } = require("apollo-server-lambda");
 const typeDefs = require("../src/graphql/typeDefs");
 const resolvers = require("../src/graphql/resolvers");
-const { connection: dbConnection } = require("../src/database/util");
-
-dbConnection();
+const mongoose = require("mongoose");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  playground: true,
-  //introspection: true,
+  playground: true, // enable GraphQL Playground IDE on prod env
 });
 
-exports.handler = server.createHandler({
-  // cors: {
-  //   origin: "*",
-  //   credentials: true,
-  // },
-});
+mongoose.connect(process.env.MONGO_DB_URL);
+
+exports.handler = server.createHandler();
